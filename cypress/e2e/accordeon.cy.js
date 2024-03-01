fc4773b77ff645718c915800ec822b91269bb076
Farfadeli <ndaunac@gmail.com>
2024-03-01 10:30:28 +0100

    Accordeon testing

---
+++ cypress/e2e/accordeon.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+context('Cypher', () => { beforeEach(() => {
+    cy.visit('../../accord/index.html'); });
+    
+    it("Test Accordeon first element", () => {
+        cy.get(".first-element").click()
+        cy.get(".first-text").should("be.visible")
+        cy.get(".first-element").click()
+        cy.get(".first-text").should("not.be.visible")
+    })
+
+    it("Test accordeon second element", () => {
+        cy.get(".second-element").click()
+        cy.get(".second-text").should("be.visible")
+        cy.get(".second-element").click()
+        cy.get(".second-text").should("not.be.visible")
+    })
+
+    it("Test have rotate" , () => {
+        cy.get(".first-element").click()
+        cy.get(".svg-1").should("have.class", "rotate-90")
+        cy.get(".second-element").click()
+        cy.get(".svg-2").should("have.class", "rotate-90")
+    })
+
+})
--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
-context('Hello World!', () => { beforeEach(() => {
+context('Cypher', () => { beforeEach(() => {
     cy.visit('../../app/index.html'); });
-    it('p should have text Hello World!', () => { cy.get('p')
-    .should('have.text', 'Hello World!'); });
+    it('.type() in cypher and in text, click on button and verif cypher encoded string', () => {
+        cy.get("input[type='number']").type('6')
+        cy.get("input[type='text']").type('Hello World')
+        cy.get(".btn").click()
+
+        cy.get("#Result").should("have.text" , "Nkrru Cuxrj")
     })
+})
