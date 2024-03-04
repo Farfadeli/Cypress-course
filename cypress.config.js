1bfe02710e99aff0d30fc11540596347696be160
Nicolas Daunac <Nicolas.Daunac@brl.fr>
2024-03-04 13:38:08 +0100

    add all lightbox test

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-    excludeSpecPattern: 
-    ["**/example/1-getting-started/*.cy.js",
-    "**/example/2-advanced-examples/*.cy.js"],
     setupNodeEvents(on, config) {
-      
+      // implement node event listeners here
     },
   },
-});
+};
